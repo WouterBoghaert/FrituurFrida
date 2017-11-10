c763f4a039df09791847753f8fe716a3e6c8499b
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2017-11-10 21:43:00 +0100

    Oefening hoofdstuk 24

--- src/be/vdab/filters/StatistiekRequestFilter.java
+++ src/be/vdab/filters/StatistiekRequestFilter.java
@@ -1,4 +1,56 @@
-package be.vdab.listeners;
+package be.vdab.filters;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter("*.htm")
+public class StatistiekRequestFilter implements Filter {
+	private final static String STATISTIEK = "statistiek";
+
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		fConfig.getServletContext().setAttribute(STATISTIEK, 
+	    		new ConcurrentHashMap<String, AtomicInteger>());
+	}
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if (request instanceof HttpServletRequest) {
+			String url = ((HttpServletRequest)(request)).getRequestURI();
+    		int index = url.indexOf(";jsessionid=");
+    		if (index != -1) {
+    			url = url.substring(0, index);
+    		}
+    		@SuppressWarnings("unchecked")
+    		ConcurrentHashMap<String, AtomicInteger> statistiek = 
+    			(ConcurrentHashMap<String, AtomicInteger>) request.getServletContext()
+    			.getAttribute(STATISTIEK);
+    		AtomicInteger aantalReedsAanwezig = 
+				statistiek.putIfAbsent(url, new AtomicInteger(1));
+    		if(aantalReedsAanwezig != null) {
+    			aantalReedsAanwezig.incrementAndGet();
+    		}
+		}
+		chain.doFilter(request, response);
+	}
+	
+	@Override
+	public void destroy() {
+		
+	}
+
+	/*
+	 * package be.vdab.listeners;
 
 import java.util.Arrays;
 import java.util.Set;
@@ -75,4 +127,9 @@ public class RequestStatistiekListener implements ServletContextListener, Servle
 
     
 	
+}
+
+	 */
+	
+
 }
